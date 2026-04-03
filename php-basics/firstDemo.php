93c351529551d3206dea2788ec9873bdd348a371
zemedkunworkalem32 <zamadeutopia@gmail.com>
2026-04-03 23:34:43 +0300

    add php basics

---
+++ php-basics/firstDemo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Demo</title>
+</head>
+<body>
+
+<h1>Welcome</h1>
+
+<?php
+echo "Hello, this is PHP!";
+?>
+
+<p>This is normal HTML text.</p>
+
+<?php
+$name = "Zemedkun";
+echo "My name is " . $name;
+?>
+
+</body>
+</html>
