40fb59b329f6659fce67b4e10dd8b2bc8f2037ed
zemedkunworkalem32 <zamadeutopia@gmail.com>
2026-04-04 10:41:49 +0300

    add print stmt

---
+++ php-basics/printStmt/echoDemo.php
@@ -0,0 +1,10 @@
+<?php
+// Single string
+echo "Hello World!<br>";
+
+// Multiple strings
+echo "PHP ", "is ", "fun!<br>";
+
+// With parentheses
+echo("This is echo with parentheses.<br>");
+?>
---
+++ php-basics/printStmt/printDemo.php
@@ -0,0 +1,11 @@
+<?php
+// Simple usage
+print "Hello World!<br>";
+
+// Using return value in expression
+$result = print "Hello again!<br>"; // outputs the string and $result = 1
+echo "Return value of print is: $result<br>";
+
+// With parentheses
+print("Using print with parentheses.<br>");
+?>
